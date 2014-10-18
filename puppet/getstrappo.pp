09e35b549f0d1031324a03ef1063640d2b96c367
Matteo Landi <matteo@matteolandi.net>
2014-10-18 13:31:21 -0400

    Add main puppet files fro analytics, api and getstrappo

---
+++ puppet/getstrappo.pp
@@ -0,0 +1,26 @@
+include mercurial
+include nginx
+include redis
+include python
+include supervisor
+
+Exec {
+  path => [ "/usr/local/sbin", "/usr/local/bin", "/usr/sbin", "/usr/bin", "/sbin", "/bin", ],
+}
+
+exec { "add_${user}_to_group_www-data":
+  command => "usermod -a -G www-data ${user}",
+  unless => "id ${user} | grep www-data",
+  require => Package[nginx]
+}
+
+nginx::gunicorn {'nginx':
+  appname => $appname,
+  appport => $appport,
+  servername => $servername,
+}
+
+supervisor::gunicorn {'supervisor-gunicorn':
+  appname => $appname,
+  user => $user,
+}
