7cc24a5eb622ef6cdf55418f67fb96e240a42c71
Matteo Landi <matteo@matteolandi.net>
2014-10-27 21:53:44 -0400

    Add ssl support to strappo-analytics and strappo-api

--- puppet/strappo-analytics.pp
+++ puppet/strappo-analytics.pp
@@ -14,10 +14,40 @@ exec { "add_${user}_to_group_www-data":
   require => Package[nginx]
 }
 
+file {"/srv/ssl/$appname":
+  ensure => 'directory',
+  owner => 'www-data',
+  group => 'www-data'
+}
+
+file { "/srv/ssl/$appname/getstrappo.com.combined.crt":
+  source => 'puppet:///modules/local/getstrappo.com.combined.crt',
+  mode => 0400,
+  owner => 'www-data',
+  group => 'www-data',
+  require => File["/srv/ssl/$appname"]
+}
+
+file { '/srv/ssl/getstrappo.com.key':
+  source => 'puppet:///modules/local/getstrappo.com.key',
+  mode => 0400,
+  owner => 'www-data',
+  group => 'www-data',
+  require => File["/srv/ssl/$appname"]
+}
+
 nginx::gunicorn {'nginx':
   appname => $appname,
   appport => $appport,
   servername => $servername,
+}
+
+nginx::gunicornssl {'nginx-ssl':
+  appname => $appname,
+  appport => $appport,
+  servername => $servername,
+  sslcert => '/srv/ssl/getstrappo.com.combined.crt',
+  sslcertkey => '/srv/ssl/getstrappo.com.key'
 }
 
 supervisor::gunicorn {'supervisor-gunicorn':
