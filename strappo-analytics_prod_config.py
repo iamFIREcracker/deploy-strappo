6363e4294963b38f41c343b79db5468c9f409fd7
Matteo Landi <matteo@matteolandi.net>
2015-01-06 20:15:35 +0100

    strappo-analytics config files

--- strappo-analytics_prod_config.py
+++ strappo-analytics_prod_config.py
@@ -4,9 +4,6 @@
 import web
 
 
-SECRET = 'itsfuckingcrazycrazy'
-
-
 web.config.LOG_SMTP_SERVER = 'smtp.mailgun.org'
 web.config.LOG_SMTP_PORT = 587
 web.config.LOG_SMTP_USERNAME = 'postmaster@sandbox564a36facd7d4e36b6f92109835f80fb.mailgun.org'
@@ -14,3 +11,10 @@ web.config.LOG_SMTP_PASSWORD = '1fbff3207b9ef57bdf6fe357c9fa7f92'
 web.config.LOG_FROM = 'noreply@analytics.getstrappo.com'
 web.config.LOG_TO = ['matteo@matteolandi.net', 'bianchigiova@gmail.com']
 web.config.LOG_SUBJECT = 'Internal Server Error on analytics.getstrappo.com'
+
+SECRET = 'itsfuckingcrazycrazy'
+
+TITANIUM_KEY = '2xnkPkNJg2qa4zzLbVdNFZVWRVBHJo40'
+TITANIUM_LOGIN = 'notifications'
+TITANIUM_PASSWORD = 'notificationsisstrongenough'
+TITANIUM_NOTIFICATION_CHANNEL = 'channel'
